ea056993740e1e9a4da677b47c91b3da6708d282
Kabeer Ahmad <kab6168@gmail.com>
2024-08-05 01:38:05 +0500

    Cooked

--- methods.php
+++ methods.php
@@ -5,3 +5,12 @@ $shit = ['arrays', 'are', 'shit'];
 $uppercased = array_map(function($shits) {
     print_r(strtoupper($shits));
 }, $shit);
+
+$iwillBe = ["filter", 'method'];
+
+$filteredArray = array_filter($iwillBe, function($item) {
+    return strtolower($item) === 'filter';
+});
+
+print_r($filteredArray);
+?>
